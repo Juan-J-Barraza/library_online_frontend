306a89862c017c0c9d7d0fc7300a69972ccef172
Juan Barraza <juanbarraza2004@outlook.com>
2026-03-13 23:22:09 -0500

    refactor: reemplazar window.alert con sistema global de notificaciones toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToastComponent } from './components/toast/toast.component';
 
 import { AuthInterceptor } from 'src/core/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToastComponent
   ],
   imports: [
     BrowserModule,
